f680038ac4f05a65f507d8ecec40c93296666a7f
Leo Oliveira <114103512+leomalefico@users.noreply.github.com>
2024-03-14 18:24:25 -0300

    Update Top_10_Vulnerabilidades-severidade-VPR.py

--- Top_10_Vulnerabilidades-severidade-VPR.py
+++ Top_10_Vulnerabilidades-severidade-VPR.py
@@ -1,63 +1,48 @@
 import json
-import os
-import requests
+from tenable.io import TenableIO
 
+# Carregar chaves da API a partir de um arquivo JSON
 with open("api_keys.json", "r") as f:
     api_keys = json.load(f)
 
-url = "https://cloud.tenable.com/workbenches/vulnerabilities"
+# Mapeamento de severidade para texto descritivo
 severity_mapping = {4: "Critical", 3: "High", 2: "Medium", 1: "Low", 0: "Info"}
 
+# Iterar sobre os clientes e suas chaves API
 for client, keys in api_keys.items():
     print(f"Client: {client}")
+    
+    # Obter as chaves de acesso
+    access_key = keys['accessKey']
+    secret_key = keys['secretKey']
+    
+    # Criar uma instância da TenableIO com as chaves de acesso
+    tio = TenableIO(access_key=access_key, secret_key=secret_key)
 
-    headers = {
-        "accept": "application/json",
-        "X-ApiKeys": f"accessKey={keys['accessKey']};secretKey={keys['secretKey']}"
-    }
-
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        
-        vulnerabilities = []  
-        for vulnerability in data["vulnerabilities"]:
-            severity = vulnerability["severity"]
-            
-            if severity == 4:  # Filtro para identificar qual severidade deseja listar
+    # Recuperar vulnerabilidades usando o método `vulns` do objeto WorkbenchesAPI
+    vulnerabilities = tio.workbenches.vulns()
+    
+    # Filtrar e classificar as vulnerabilidades
+    sorted_vulnerabilities = []
+    for vuln in vulnerabilities:
+        # Filtro para qual severidade deseja listar
+        if vuln["severity"] >= 4:  
+            vpr_score = vuln.get('vpr_score', None)
+            # Filtro para identificar qual score deseja listar
+            if vpr_score is not None and vpr_score >= 10:  
+                sorted_vulnerabilities.append(vuln)
                 
-                count = vulnerability['count']
-                plugin_id = vulnerability["plugin_id"]
-                plugin_name = vulnerability["plugin_name"]
-                vulnerability_state = vulnerability["vulnerability_state"]
-                
-                # Verifica se a chave 'vpr_score' está presente antes de acessá-la
-                vpr_score = vulnerability.get('vpr_score', None)
-                
-                if vpr_score is not None and vpr_score >= 9.9: # Filtro para identificar qual score deseja listar
-                    vulnerabilities.append({
-                        "plugin_id": plugin_id,
-                        "plugin_name": plugin_name,
-                        "vulnerability_state": vulnerability_state,
-                        "severity": severity,
-                        "vpr_score": vpr_score,
-                        "count": count
-                    })
-
-        # Ordenar a lista de vulnerabilidades com base no número de contagens (count)
-        sorted_vulnerabilities = sorted(vulnerabilities, key=lambda x: x["count"], reverse=True)
-        
-        
+    # Ordenar as vulnerabilidades por contagem
+    sorted_vulnerabilities = sorted(sorted_vulnerabilities, key=lambda x: x["count"], reverse=True)
+    
+    # Verificar se há vulnerabilidades para exibir
+    if not sorted_vulnerabilities:
+        print("Não foram encontradas vulnerabilidades ativas para este VPR / Severidade")
+        print("\n")
+    else:
         print("Top 10 Vulnerabilities:")
+        # Exibir as 10 principais vulnerabilidades
         for idx, vulnerability in enumerate(sorted_vulnerabilities[:10], 1):
             severity_text = severity_mapping.get(vulnerability["severity"], "Unknown")
-            print(f"{idx}. Plugin ID: {vulnerability['plugin_id']}")
-            print(f"   Plugin Name: {vulnerability['plugin_name']}")
-            print(f"   Vulnerability State: {vulnerability['vulnerability_state']}")
-            print(f"   Severity: {severity_text}")
-            print(f"   VPR Score: {vulnerability.get('vpr_score', 'N/A')}")
-            print(f"   Count: {vulnerability['count']}")
-            print("-" * 50)
-        print("\n")  
-    else:
-        print(f"Failed to fetch data for client: {client}. Status code: {response.status_code}")
+            print(f"Plugin ID: {vulnerability['plugin_id']}, Plugin Name: {vulnerability['plugin_name']}, Vulnerability State: {vulnerability['vulnerability_state']}, Severity: {severity_text}, VPR Score: {vulnerability.get('vpr_score', 'N/A')}, Count: {vulnerability['count']}")
+        print("\n")
